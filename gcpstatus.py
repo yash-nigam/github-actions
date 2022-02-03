8825237271b79b6e0b84192f98a786cf14959c9d
yash-nigam <yashnigam@gmail.com>
2022-02-03 19:04:22 +0530

    Update gcpstatus.py

--- gcpstatus.py
+++ gcpstatus.py
@@ -1,6 +1,7 @@
 import os
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
 
 
 URL = "https://status.cloud.google.com/"
@@ -8,6 +9,7 @@ page = requests.get(URL)
 soup = BeautifulSoup(page.content, "html.parser")
 results = soup.find("tbody")
 services = results.find_all("tr", class_="product-row")
+print(services)
 
 for service in services:
   product_name=service.find("td",class_="product-name")
@@ -15,3 +17,12 @@ for service in services:
   product_class=product["class"]
   product_status=" ".join(product_class[1:])
   print(product_name.text,product_status)
+
+s=[]
+for service in services:
+  service_name=service.find("td",class_="product-name").text
+  service_status=service.find("a",class_="status-icon")["class"][1]
+  s.append([service_name,service_status,''])
+
+print(s)
+pd.DataFrame(s).to_excel("service_status.xlsx",header=["service_name","status","last_refresh_date"],index=False)
